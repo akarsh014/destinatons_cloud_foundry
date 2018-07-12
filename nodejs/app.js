f0347baf762eb66493e4b3c025c15d6044b29f86
System Administrator <akarsh.sidhartha@sap.com>
2018-07-12 14:57:12 +0530

    First Commit

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,12 +3,21 @@ var express = require('express');
 var cfenv = require("cfenv");
 
 var route = require('./routes/route');
+const users = require('./users.json');
 
 let app = express();
 // passport.use(new JWTStrategy(xsenv.getServices({uaa:{tag:'xsuaa'}}).uaa));
 // app.use(passport.initialize());
 // app.use(passport.authenticate('JWT', { session: false }));
-app.use('/', route);
+//app.use('/', route);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/users',function(req, res){
+  res.status(200).json(users);
+});
 
 let appEnv   = cfenv.getAppEnv();
 app.listen(appEnv.port, function() {
