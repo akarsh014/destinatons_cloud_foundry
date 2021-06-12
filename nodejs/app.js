ad4a6eed35c171bbb63c60e8d38d87d0237cc1f5
Lautaro Capella <84327226+lautarocapella@users.noreply.github.com>
2021-06-12 17:22:27 -0300

    Removed unused confusing code

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,14 +2,9 @@
 var express = require('express');
 var cfenv = require("cfenv");
 
-var route = require('./routes/route');
 const users = require('./users.json');
 
 let app = express();
-// passport.use(new JWTStrategy(xsenv.getServices({uaa:{tag:'xsuaa'}}).uaa));
-// app.use(passport.initialize());
-// app.use(passport.authenticate('JWT', { session: false }));
-//app.use('/', route);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
